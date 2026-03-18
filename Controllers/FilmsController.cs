dd351a707f188f414146999e262994ee26ff517b
112 <112@112-03>
2026-03-18 09:26:20 +0100

    ViewComponents, CategoryFilms

---
+++ Controllers/FilmsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SklepPB.DAL;
+
+namespace SklepPB.Controllers
+{
+    public class FilmsController : Controller
+    {
+
+        FilmsContext db;
+
+        public FilmsController(FilmsContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryFilms(string categoryName)
+        {
+            var category = db.Categories.Include("Films").Where(c => c.Name.ToUpper() == categoryName.ToUpper()).Single();
+
+            var films = category.Films.ToList();
+
+            return View(films);
+        }
+    }
+}
---
+++ ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepPB.DAL;
+
+namespace SklepPB.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        FilmsContext db;
+
+        public MenuViewComponent(FilmsContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("_Menu", db.Categories.ToList()));
+        }
+    }
+}
